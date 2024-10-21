8fb85e1d9c9667861f3aef0b17ad582f971668eb
Ayomide Ajayi <lancerajayi@yahoo.com>
2024-10-21 14:47:02 +0100

    Amendement

--- Main.tf
+++ Main.tf
@@ -89,26 +89,20 @@ resource "aws_route_table_association" "rt_associate_public3" {
   route_table_id = aws_route_table.rt_pub.id
 
 }
-
-
 #create Private Subnets 1, 2 & 3
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "eu-west-2a"
-
-
   tags = {
     Name = "MS-Cleaning-PriSub 1"
   }
 }
-
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = "eu-west-2b"
-
   tags = {
     Name = "MS-Cleaning-PriSub 2"
   }
@@ -118,42 +112,33 @@ resource "aws_subnet" "private_subnet_3" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.5.0/24"
   availability_zone = "eu-west-2c"
-
-
   tags = {
     Name = "MS-Cleaning-PriSub 3"
   }
 }
 
 #Create Routing Table for Private Subnets
-
-resource "aws_route_table" "rt_pri" {
+resource "aws_route_table" "rt_private" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "10.0.0.0/16" 
     gateway_id = "local"
   }
-
   tags = {
     Name = "Prisub RT"
   }
-
 }
-
-
 # Associate Prisub RT to Private Subnets 1, 2 and 3
 
 resource "aws_route_table_association" "rt_associate_private1" {
   subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.rt_pri.id
+  route_table_id = aws_route_table.rt_private.id
 }
-
 resource "aws_route_table_association" "rt_associate_private2" {
   subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.rt_pri.id
+  route_table_id = aws_route_table.rt_private.id
 }
 resource "aws_route_table_association" "rt_associate_private3" {
   subnet_id      = aws_subnet.private_subnet_3.id
-  route_table_id = aws_route_table.rt_pri.id
-
+  route_table_id = aws_route_table.rt_private.id
 }
